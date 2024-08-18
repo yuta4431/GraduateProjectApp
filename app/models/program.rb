89d2751cf01b29db61bc85d5606cdd19a9bf9396
HayashiYuta <orange_1272@yahoo.co.jp>
2024-08-19 06:26:07 +0900

    番組メモ投稿・一覧・更新・削除機能&画面

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,11 +1,12 @@
 class Program < ApplicationRecord
   has_many :hosts
+  has_many :posts
   belongs_to :user, optional: true
 
-  validates :title, presence: true, uniqueness: true, length: { maximum: 100 }
-  validates :host_name, presence: true, length: { maximum: 100 }
+  validates :title, presence: true, uniqueness: true, length: { maximum: 255 }
+  validates :host_name, presence: true, length: { maximum: 255 }
   validates :channel, presence: true
-  validates :day_of_week, presence: true, length: { maximum: 20 }
+  validates :day_of_week, presence: true, length: { maximum: 255 }
   validates :start_time, presence: true
   validates :end_time, presence: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'hosts/show'
   get 'login', to: 'user_sessions#new', as: 'login'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy', as: 'logout'
   
   resources :users, only: %i[new create]
-  resources :hosts
+  resources :hosts, only: %i[show]
   resources :programs
+  resources :posts
   # Defines the root path route ("/")
-  root "programs#index"
+  root "posts#index"
 end
+ 
