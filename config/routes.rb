7b3c7725f4e777824a6cc0f588a8bfc110a2b105
HayashiYuta <orange_1272@yahoo.co.jp>
2024-08-19 00:25:01 +0900

    ログイン機能&画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'login', to: 'user_sessions#new', as: 'login'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy', as: 'logout'
+  
   resources :users, only: %i[new create]
   # Defines the root path route ("/")
   # root "articles#index"
