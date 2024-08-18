175ef147f9d9bee2bda7cad1b4b542c4b156d570
HayashiYuta <orange_1272@yahoo.co.jp>
2024-08-18 23:43:24 +0900

    ユーザー登録機能&画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path, success: "ユーザー登録が完了しました"
+      redirect_to new_user_path, success: "ユーザー登録が完了しました"
     else
       flash.now[:danger] = "ユーザー登録に失敗しました"
-      render :new,  status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
